c18586ffc4df61b037c0f1992a6a7553f00e063e
manuelreis <manuel.barreto.reis@gmail.com>
2015-03-10 18:33:16 +0000

    modified the parser to explore multiple dimensions of the lexicon, if any. And modified the output format

--- parser-spark/dataset_us_filter_and_score_calc.py
+++ parser-spark/dataset_us_filter_and_score_calc.py
@@ -10,9 +10,10 @@ outFileProcessedDataset = "/tmp/ist169518/filtered-scored"
 
 # Emotional Lexicons
 # csv files with first column with the word, and second with the score separated with commas
+
 word_list_info = [
-  {'key': 'en.hedo' , 'path': '/Users/manuelreis/Downloads/hedonometer_anew_full.csv' },
-  {'key': 'es.anew' , 'path': '/Users/manuelreis/Downloads/es_anew_all.csv'}
+  {'key': 'en.hedo' , 'path': '/afs/.ist.utl.pt/users/1/8/ist169518/lexicons/es_anew_all.csv' , 'dimensions' : [{'name':'happiness', 'index':'1'}]},
+  {'key': 'es.anew' , 'path': '/afs/.ist.utl.pt/users/1/8/ist169518/lexicons/es_anew_all.csv', 'dimensions' : [{'name':'valence', 'index':1}, {'name':'arousal', 'index':2}, {'name':'dominance', 'index':3}]}
 ]
 
 #The set of considered filters appliable to the lexicons. Note that every enty on this set must have an associated method whose name starts by "score_filter_", e.g., def score_filter_MYFILTER: ...
@@ -23,7 +24,7 @@ filters = ['more_than_7', 'delta_one_of_5', 'no_filter']
 
 # functions that filters the words in the list accordingly to some parameter
 def score_filter_more_than_7(score):
-  return score > 7
+    return score > 7
 
 def score_filter_delta_one_of_5(score):
     return  score < 4 or score > 6
@@ -31,8 +32,9 @@ def score_filter_delta_one_of_5(score):
 def score_filter_no_filter(score):
     return True
 
-
+#####################################
 ### DO NOT MODIFY BELOW THIS LINE ###
+#####################################
 
 def process_tweets(line):
     tweet = json.loads(line)
@@ -49,25 +51,31 @@ def calculate_emotional_score(tweet):
     tweet_words = tweet['text'].lower().split()
     for word_lists_key in word_lists_var.value.keys():
         choosen_record = {'total_words': 0}
+        
         for word_list in word_lists_var.value[word_lists_key]:
-            record = {'word_list_key': word_list['key'], 'total_word_occurences': dict(), 'total_words': 0}
-            for word in tweet_words:
-                score = word_list['word_list'].get(word, None)
-                if score is not None:
-                    record['total_words'] += 1
-                    total_word_occurences = record['total_word_occurences'].get(word, None)
-                    if total_word_occurences is None:
-                        record['total_word_occurences'][word] = {'occurences': 1 ,'score': score}
-                    else:
-                        total_word_occurences['occurences'] += 1
+            record = {'word_list_key': word_list['key'],  'dimensions': list(), 'total_words': 0}
+            for dimension in word_list['dimensions'].keys():
+                dimension_record = {'name': dimension, 'total_word_occurences': dict()}
+                for word in tweet_words:
+                    score = word_list['dimensions'][dimension].get(word, None)
+                    if score is not None:
+                        record['total_words'] += 1
+                        total_word_occurences = dimension_record['total_word_occurences'].get(word, None)
+                        if total_word_occurences is None:
+                            dimension_record['total_word_occurences'][word] = {'occurences': 1 ,'score': score}
+                        else:
+                            dimension_record['total_word_occurences'][word]['occurences'] += 1
+                record['dimensions'].append(dimension_record)
             if record['total_words'] > choosen_record['total_words']: choosen_record = record
+        
         if choosen_record['total_words'] > 0:
             ok_to_output = True
-            total_score = 0
-            for term in choosen_record['total_word_occurences'].values():
-                total_score += term['occurences'] * term['score']
-            score = format(total_score / choosen_record['total_words'], '.2f')
-            tweet['scores'][word_lists_key] = {'score': score, 'word_list_key': choosen_record['word_list_key'], 'word_count': choosen_record['total_words']}
+            for dim in choosen_record['dimensions']:
+                total_score = 0
+                for term in dim['total_word_occurences'].values():
+                    total_score += term['occurences'] * term['score']
+                score = format(total_score / choosen_record['total_words'], '.2f')
+                tweet[choosen_record['word_list_key'] + "-" + dim['name'] + "-" + word_lists_key ] = {'score': score, 'word_count': choosen_record['total_words']}
 
     if ok_to_output:
         return tweet
@@ -80,7 +88,6 @@ def filtered(tweet):
     filtered_tweet['state'] = tweet['carmen']['state']
     filtered_tweet['county'] = tweet['carmen']['county']
     filtered_tweet['text'] = tweet['text']
-    filtered_tweet['scores'] = dict()
     return filtered_tweet
 
 states = ["alabama","arizona","arkansas","california","colorado","connecticut","delaware","florida","georgia","idaho","illinois","indiana","iowa","kansas","kentucky","louisiana","maine","maryland","massachusetts","michigan","minnesota","mississippi","missouri","montana","nebraska","nevada","new hampshire","new jersey","new mexico","new york","north carolina","north dakota","ohio","oklahoma","oregon","pennsylvania","rhode island","south carolina","south dakota","tennessee","texas","utah","vermont","virginia","washington","west virginia","wisconsin","wyoming"]
@@ -105,16 +112,23 @@ for info in word_list_info:
     temporary_word_lists = dict()
     for filter_name in filters:
         temporary_word_lists[filter_name] = dict()
+        temporary_word_lists[filter_name] = dict()
+        for dimension in info['dimensions']:
+            temporary_word_lists[filter_name][dimension['name']] = dict()
+
+
     with open(info['path'], "r") as fp:
         for line in fp:
             row = line.split(',')
             word = row[0].lower().decode('utf-8')
-            score = float(row[1])
-            for filter_name in filters:
-                if globals()["score_filter_" + filter_name](score):
-                    temporary_word_lists[filter_name][word] = score
+            for dimension in info['dimensions']:
+
+                score = float(row[int(dimension['index'])])
+                for filter_name in filters:
+                    if globals()["score_filter_" + filter_name](score):
+                        temporary_word_lists[filter_name][dimension['name']][word] = score
     for filter_name in filters:
-        word_lists[filter_name].append({'key': info['key'], 'word_list': temporary_word_lists[filter_name]})
+        word_lists[filter_name].append({'key': info['key'], 'dimensions': temporary_word_lists[filter_name]})
 
 
 '''Spark Code'''
