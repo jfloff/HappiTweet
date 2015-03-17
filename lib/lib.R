0f78f80598b52d662752d0b0a5753a76b6df6022
jfloff <jfloff@gmail.com>
2015-03-17 12:12:31 +0000

    Rename folder for ruby parser. Update tweets_count_features to work with multiple lexicons

--- lib/lib.R
+++ lib/lib.R
@@ -7,17 +7,17 @@ library(RJSONIO)
 # Parses a file with tweets building a CSV with 3 columns for each tweet: state, county and score
 # input_filename -> name of file with the tweets
 #
-to_state_county_score <- function(input_filename){
+to_state_county_score = function(input_filename){
   # Open connection to file
   con  = file(input_filename, open = "r")
   
   # keeps a data frame per lexicon
-  tweets_by_lexicon <- list()
+  tweets_by_lexicon = list()
   # i = 0
   # parse json file by line
   while (length(line <- readLines(con, n = 1, warn = FALSE)) > 0)
   {
-    tweet <- fromJSON(line)
+    tweet = fromJSON(line)
 
     # for each lexicon in the tweet checks if its in the list
     # it not creates an empty dataframe
@@ -51,13 +51,13 @@ to_state_county_score <- function(input_filename){
 
 ####
 # Mode of an array of scores
-mode <- function(x) {
+mode = function(x) {
   if(length(x) < 2) {
     x
   }
   else {
     # limits and adjust should be changed to meet expectations
-    d <- density(x, from=0, to=100 , adjust = 0.805)
+    d = density(x, from=0, to=100 , adjust = 0.805)
     d$x[which.max(d$y)]
   }
 }
@@ -69,7 +69,7 @@ mode <- function(x) {
 # file      -> file with tweets' score with state and county info
 # by_state  -> flag that indicates if the features are by state (true) or county (false)
 #
-score_features <- function(file, by_state) {
+score_features = function(file, by_state) {
   
   scores_by_lexicon = to_state_county_score(file)
   
@@ -104,89 +104,160 @@ score_features <- function(file, by_state) {
 }
 
 
-
-#####################################################################################
-###################### IN NEED OF REVSION FROM THIS POINT BELOW ##################### 
-#####################################################################################
-
+####
+# Count tweets in a CSV file by state or county
+#   file      -> CSV file that has tweets with a columns: 'state', 'county'
+#   by_state  -> flag that indicates if we wnt to count by state or county
+#
+count_tweets_all = function(input_filename, by_state) {
+  # Open connection to file
+  con  = file(input_filename, open = "r")
+  # store the data so after we build a data frame
+  states = c()
+  counties = c()
+  # parse json file by line
+  # i = 0
+  while (length(line <- readLines(con, n = 1, warn = FALSE)) > 0)
+  {
+    tweet = fromJSON(line)
+    
+    states = c(tweet[['state']], states)
+    counties = c(tweet[['county']], counties)
+    
+    # i = i + 1
+    # if (i == 100) break
+  }
+  close(con)
+  # build data frame and write to CSV
+  state_county = data.frame(state = states, county = counties)
+  
+  if(by_state){
+    df = count(state_county, c('state'))
+  } else {
+    # state_county concat to form unique "id"
+    state_county$state_county = paste(state_county$state, state_county$county, sep=",")
+    # count by state_county
+    df = count(state_county, c('state_county'))
+  }
+  names(df) = c('entity', 'count')
+  df
+}
 
 ####
 # Count tweets in a CSV file by state or county
 #   file      -> CSV file that has tweets with a columns: 'state', 'county'
 #   by_state  -> flag that indicates if we wnt to count by state or county
 #
-count_tweets <- function(input_filename, by_state) {
+count_tweets_by_lexicon = function(input_filename, by_state) {
   # Open connection to file
-  con  <- file(input_filename, open = "r")
-  # store the data so after we build a data frame
-  states <- c()
-  counties <- c()
+  con = file(input_filename, open = "r")
+  
+  # keeps a data frame per lexicon
+  tweets_by_lexicon = list()
+  # i = 0
   # parse json file by line
   while (length(line <- readLines(con, n = 1, warn = FALSE)) > 0)
   {
-    tweet <- fromJSON(line)
-
-    states <- c(tweet[['state']], states)
-    counties <- c(tweet[['county']], counties)
+    tweet = fromJSON(line)
+    
+    # for each lexicon in the tweet checks if its in the list
+    # it not creates an empty dataframe
+    for(score in tweet[['scores']])
+    {
+      # gets the name of the lexicon
+      lexicon = names(score)
+      
+      # if lexicon was not initiated, creates a new entry and puts an empty dataframe in it
+      if(is.null(tweets_by_lexicon[[lexicon]]))
+      {
+        df = data.frame(state=character(), county=character(), state_county=character(), stringsAsFactors=FALSE)
+        tweets_by_lexicon[[lexicon]] = df
+      }
+      
+      # new row values. Order = state,county
+      newrow = c(tweet[['state']], tweet[['county']], paste(tweet[['state']], tweet[['county']], sep=","))
+      # adds to end of the data frame
+      tweets_by_lexicon[[lexicon]][nrow(tweets_by_lexicon[[lexicon]])+1,] = newrow
+    }
+    
+    # i = i + 1
+    # if (i == 100) break
   }
+  
+  # close file connection
   close(con)
-  # build data frame and write to CSV
-  state_county <- data.frame(state = states, county = counties)
-
-  if(by_state){
-    df <- count(state_county, c('state'))
-  } else {
-    # state_county concat to form unique "id"
-    state_county$state_county = paste(state_county$state, state_county$county, sep=",")
-    # count by state_county
-    df <- count(state_county, c('state_county'))
+  
+  # for each lexicon chooses its 'entity'
+  for(lexicon in names(tweets_by_lexicon))
+  {
+    # filter is different for each entity
+    filter = if(by_state) c('state') else c('state_county')
+    # count tweets by filter
+    df = count(tweets_by_lexicon[[lexicon]], filter)
+    # change names
+    names(df) = c('entity', 'count')
+    # replace on list of tweets_by_lexicon
+    tweets_by_lexicon[[lexicon]] = df
   }
-  names(df) <- c('entity', 'count')
-  df
+  
+  # returns built df
+  return(tweets_by_lexicon)
 }
-
 
 ####
 # Returns features related to the number of tweets per entity
 #
 # file      -> file with tweets' score with state and county info
+# all_file  -> file with state and county for all the tweets geolocated
 # by_state  -> flag that indicates if the features are by state (true) or county (false)
 #
-tweets_count_features <- function(file, all_file, by_state) {
-  df <- count_tweets(file, by_state)
-  # Open csv with all tweets with state and county
-  all_tweets <- count_tweets(all_file, by_state)
-  # merge data replacing NAs with 0
-  tweets <- merge(df, all_tweets, by="entity", all = TRUE)
-  tweets[is.na(tweets)] <- 0
+tweets_count_features = function(file, all_file, by_state) {
+  # counts tweets by each lexicon
+  tweets_by_lexicon = count_tweets_by_lexicon(file, by_state)
+  # counts all tweets with state and county
+  all_tweets = count_tweets_all(all_file, by_state)
   
-  tweets$percentage <- round((tweets$count.x * 100)/tweets$count.y, digits=2)
-  res <- data.frame(entity=tweets$entity, count=tweets$count.x, percentage=tweets$percentage)
+  # for each lexicon merges with the total and calculates percentage
+  for(lexicon in names(tweets_by_lexicon))
+  {
+    df = tweets_by_lexicon[[lexicon]]
+    
+    # merge data replacing NAs with 0
+    tweets = merge(df, all_tweets, by="entity", all = TRUE)
+    tweets[is.na(tweets)] = 0
+    # calculate percentage
+    tweets$percentage = round((tweets$count.x * 100)/tweets$count.y, digits=2)
+    res = data.frame(entity=tweets$entity, count=tweets$count.x, percentage=tweets$percentage)
+    
+    tweets_by_lexicon[[lexicon]] = res[with(res, order(entity)), ]
+  }
   
-  res[with(res, order(entity)), ]
-  res
+  return(tweets_by_lexicon)
 }
 
+#####################################################################################
+###################### IN NEED OF REVSION FROM THIS POINT BELOW ##################### 
+#####################################################################################
 
 ####
 # Parses a file with tweets building a CSV with 3 columns for each tweet: state, county and word_count
 # input_filename -> name of file with the tweets
 #
-to_state_county_word_count <- function(input_filename){
+to_state_county_word_count = function(input_filename){
   # Open connection to file
-  con  <- file(input_filename, open = "r")
+  con  = file(input_filename, open = "r")
   # store the data so after we build a data frame
-  states <- c()
-  counties <- c()
-  word_counts <- c()
+  states = c()
+  counties = c()
+  word_counts = c()
   # parse json file by line
-  while (length(line <- readLines(con, n = 1, warn = FALSE)) > 0)
+  while (length(line <-readLines(con, n = 1, warn = FALSE)) > 0)
   {
-    tweet <- fromJSON(line)
+    tweet = fromJSON(line)
 
-    states <- c(tweet[['state']], states)
-    counties <- c(tweet[['county']], counties)
-    word_counts <- c(tweet[['word_count']], word_counts)
+    states = c(tweet[['state']], states)
+    counties = c(tweet[['county']], counties)
+    word_counts = c(tweet[['word_count']], word_counts)
   }
   close(con)
   # build data frame and write to CSV
@@ -199,22 +270,22 @@ to_state_county_word_count <- function(input_filename){
 # file      -> file with tweets' word_count with state and county info
 # by_state  -> flag that indicates if the features are by state (true) or county (false)
 #
-mean_words_features <- function(file, by_state) {
-  state_county_word_count <- to_state_county_word_count(file)
+mean_words_features = function(file, by_state) {
+  state_county_word_count = to_state_county_word_count(file)
 
   if(by_state) {
-    state_county_word_count <- subset(state_county_word_count, select=c('state','word_count'))
+    state_county_word_count = subset(state_county_word_count, select=c('state','word_count'))
   } else {
     state_county_word_count$state_county = paste(state_county_word_count$state, state_county_word_count$county, sep=",")
-    state_county_word_count <- subset(state_county_word_count, select=c('state_county','word_count'))
+    state_county_word_count = subset(state_county_word_count, select=c('state_county','word_count'))
   }
-  colnames(state_county_word_count)[1] <- 'entity'
+  colnames(state_county_word_count)[1] = 'entity'
 
-  df <- aggregate(. ~ entity, data=state_county_word_count, FUN=sum)
-  colnames(df)[2] <- 'total_words'
+  df = aggregate(. ~ entity, data=state_county_word_count, FUN=sum)
+  colnames(df)[2] = 'total_words'
 
-  df$total_tweets <- count(state_county_word_count, c('entity'))$freq
-  res <- data.frame(entity = df$entity, mean_words = df$total_words / df$total_tweets)
+  df$total_tweets = count(state_county_word_count, c('entity'))$freq
+  res = data.frame(entity = df$entity, mean_words = df$total_words / df$total_tweets)
 
   res[with(res, order(entity)), ]
   res
@@ -225,14 +296,14 @@ mean_words_features <- function(file, by_state) {
 #
 # features -> list of features data frames to merge
 #
-merge_features <- function(features){
+merge_features = function(features){
   # merge the first two elements
-  merged <- merge(features[1], features[2], by="entity", all = TRUE)
+  merged = merge(features[1], features[2], by="entity", all = TRUE)
   # remove those from the features
-  features<-features[-c(1, 2)]
+  features=features[-c(1, 2)]
   # loops the remaining, merging them
   for (feature in features) {
-    merged <- merge(merged, feature, by="entity", all = TRUE)
+    merged = merge(merged, feature, by="entity", all = TRUE)
   }
   merged
 }
--- model/features.R
+++ model/features.R
@@ -8,8 +8,10 @@ source('lib/lib.R', echo=FALSE)
 
 score_features = score_features(file="huge-data/scored.json", by_state=TRUE)
 
-# features_2 = tweets_count_features(file="huge-data/scored.json", all_file="data/us_tweets", by_state=TRUE)
-# ....
+count_features = tweets_count_features(file="huge-data/scored.json", 
+                                       all_file="huge-data/all_state_county.json", 
+                                       by_state=TRUE)
+
 # features_10 <- mean_words_features(file="~/non_neutral_data_test_subset", by_state=TRUE)
 # ....
 # state_features <- merge_features(features = list(features_1, features_10))
@@ -21,11 +23,14 @@ score_features = score_features(file="huge-data/scored.json", by_state=TRUE)
 
 score_features_county = score_features(file="huge-data/scored.json", by_state=FALSE)
 
-# ....
-#features_7_county <- tweets_count_features(file="data/us_tweets_with_score", all_file="data/us_tweets", by_state=FALSE)
+count_features_county = tweets_count_features(file="huge-data/scored.json", 
+                                       all_file="huge-data/all_state_county.json", 
+                                       by_state=false)
+
 # ....
 #features_10_county <- mean_words_features(file="data/us_tweets_with_score", by_state=FALSE)
-# ....
+
+
 
 #counties_features <- merge_features(features = list(features_1_county, features_10_county, features_7_county))
 
