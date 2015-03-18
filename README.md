cf09d16c38046482c1fbc412a932db1b1d3004e7
jfloff <jfloff@gmail.com>
2015-03-18 12:51:57 +0000

    Refactor of filenames in features. Add documentation for Features.

--- README.md
+++ README.md
@@ -20,11 +20,47 @@ Results from parsing the tweet collection should be place in the `huge-data` fol
 1. From the root, run `ruby aws/task_splitter.rb` so the tasks are complete. In the *task splitter* you can change the file with the urls to be split and how many chunks that file will be split to. Open `aws/task_splitter.rb` and you'll find these settings
 2. When the script ends, multiple instances will be running dealing with your data.
 3. Periodically, run `ruby aws/checks_complete.rb` so you known the state of your instances. When all the instances seem complete move on to next step.
-4. (UNDER DEVELOPLMENT) When all your instances finish, run `ruby aws/end_tasks.rb` so all the data is tranferred to local machines, as well as stopping the AWS instances. Instances are not deleted as a safety measure.
+4. (UNDER DEVELOPLMENT) When all your instances finish, run `ruby aws/end_tasks.rb` so all the data is transferred to local machines, as well as stopping the AWS instances. Instances are not deleted as a safety measure.
 
 ##### Spark Parser
 
 
 ## Features and Model
+
+First you need to process the features for your dataset. Edit the files needed in the `feature.R` file:
+
+* `scored_tweets`: file with tweets that have a score associated. Each record should be in a separate line, and each line should have a json record like in the below example:
+```json
+	{  
+	  "county":"Conway County",
+	  "state":"Arkansas",
+	  "scores":[  
+	    {  
+	      "en.hedo-happiness-delta_one_of_5":{  
+	        "score":"8.41",
+	        "word_count":1
+	      }
+	    },
+	    {  
+	      "en.hedo-happiness-more_than_7":{  
+	        "score":"8.41",
+	        "word_count":1
+	      }
+	    }
+	  ]
+	}
+```
+
+* `all_tweets`: file with all the geolocated tweets of the dataset (regardless if they got a score from any lexicon). Each record should be in a separate line, and each line should have a json record like in the below example:
+```json
+	{  
+	  "county":"Conway County",
+	  "state":"Arkansas"
+	}
+```
+
+* `output_state`: CSV with the output of the calculated features for each US state
+
+* `output_county`: CSV with the output of the calculated features for each US county
 
 ## Charts
--- model/features.R
+++ model/features.R
@@ -2,35 +2,42 @@ setwd(Sys.getenv("R_HAPPITWEET"))
 
 source('model/lib.R', echo=FALSE)
 
+# input files
+scored_tweets = "huge-data/scored.json"
+all_tweets = "huge-data/all_state_county.json"
+# output files
+output_state = "huge-data/state_features.csv"
+output_county = "huge-data/county_features.csv"
+
 ############################################################################
 ############################## BY STATE ####################################
 ############################################################################
 
-score_features_state = score_features(file="huge-data/scored.json", by_state=TRUE)
+score_features_state = score_features(file=scored_tweets, by_state=TRUE)
 
-tweets_count_features_state = tweets_count_features(file="huge-data/scored.json", 
-                                       all_file="huge-data/all_state_county.json", 
+tweets_count_features_state = tweets_count_features(file=scored_tweets, 
+                                       all_file=all_tweets, 
                                        by_state=TRUE)
 
-mean_words_features_state = mean_words_features(file="huge-data/scored.json", by_state=TRUE)
+mean_words_features_state = mean_words_features(file=scored_tweets, by_state=TRUE)
 
 
 state_features = merge_features(list(score_features_state, tweets_count_features_state, mean_words_features_state))
 
-write.csv(file="huge-data/state_features.csv", x=state_features)
+write.csv(file=output_state, x=state_features)
 
 ############################################################################
 ############################# BY COUNTY ####################################
 ############################################################################
 
-score_features_county = score_features(file="huge-data/scored.json", by_state=FALSE)
+score_features_county = score_features(file=scored_tweets, by_state=FALSE)
 
-tweets_count_features_county = tweets_count_features(file="huge-data/scored.json", 
-                                       all_file="huge-data/all_state_county.json", 
+tweets_count_features_county = tweets_count_features(file=scored_tweets, 
+                                       all_file=all_tweets, 
                                        by_state=FALSE)
 
-mean_words_features_county = mean_words_features(file="huge-data/scored.json", by_state=FALSE)
+mean_words_features_county = mean_words_features(file=scored_tweets, by_state=FALSE)
 
 county_features = merge_features(list(score_features_county, tweets_count_features_county, mean_words_features_county))
 
-write.csv(file="huge-data/county_features.csv", x=county_features)
+write.csv(file=output_county, x=county_features)
